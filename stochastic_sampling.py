9139c5820128dc06bf5c188c3cbba3f3538575cf
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-02-14 22:04:02 -0800

    edit spacing

--- stochastic_sampling.py
+++ stochastic_sampling.py
@@ -3,11 +3,11 @@ import random
 from histogram import frequency
 
 with open("dr-seuss.txt","r") as f:
-        lines = f.read().split(" ")
-        total_word_count = len(lines)
+    lines = f.read().split(" ")
+    total_word_count = len(lines)
 
-        final_dictionary = frequency(lines)
-        print('Final Dictionary: {}\n'.format(final_dictionary))
+    final_dictionary = frequency(lines)
+    print('Final Dictionary: {}\n'.format(final_dictionary))
 
 def randomized_item(my_dict):
     print('Percentage Count: ')
@@ -19,32 +19,38 @@ randomized_item(final_dictionary)
 
 
 def probabilistic_word_sampler():
-    '''Sample words based on probablity
-Run random function on list'''
+    '''Sample words based on probablity. Run random function on list'''
     total = 0
     for key in final_dictionary:
         total += final_dictionary[key] 
         
     random_number = random.randrange(total)
     number_line_total = 0
+
     for key in final_dictionary:
         number_line_total += final_dictionary[key]
+
         if random_number < number_line_total:
             return key
 
 
 def thousand_times():
-    '''This function runs the above function 10,000 times.
-Then it creates a  histogram from resulting word'''
+    '''This function runs the above function 10,000 times. Then it creates a  histogram from resulting word'''
+
     thousand_dictionary = {}
     counter = 10000
+
     while counter > 0:
+
         random_word = probabilistic_word_sampler()
+
         if random_word in thousand_dictionary:
             thousand_dictionary[random_word] += 1
+
         else:
             thousand_dictionary[random_word] = 1
         counter -= 1
+        
     return thousand_dictionary
 
 
