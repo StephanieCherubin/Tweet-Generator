f5bdd52a2dbfcc87a032a36d2b63360014cc9b0c
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-10-27 21:53:12 -0700

    add random dictionary word file

--- random_dictionary_word.py
+++ random_dictionary_word.py
@@ -1,27 +1,26 @@
 import sys
 import random 
 
-file_name = '/usr/share/dict/words'
 
-def get_dictionary_words():
-    '''open the default dictionary
-    take an integer that sets the amount to pick
-    pick random words
-    return random words'''
-
+def get_dictionary_words(file_name = '/usr/share/dict/words'):
+    '''open the default dictionary'''
+    
     with open(file_name, 'r') as f:
         dictionary_words = f.read()
 
-    return dictionary_words.splitlines()
+    print(dictionary_words.splitlines())
 
-def create_random_sentence(dictionary, num_words):
-    for _ in range(num_words):
-        return ''.join(random.choice(dictionary))
+# def create_random_sentence(dictionary, num_words):
+#     '''take an integer that sets the amount to pick
+#     pick random words
+#     return random words'''
+#     for _ in range(num_words):
+#         return ''.join(random.choice(dictionary))
 
 def main():
-    num_words = int(sys.argv[1])
+    # num_words = int(sys.argv[1])
     dictionary = get_dictionary_words()
-    print(create_random_sentence(dictionary, num_words))
+    # print(create_random_sentence(dictionary, num_words))
 
 if __name__ == '__main__':
     main()
